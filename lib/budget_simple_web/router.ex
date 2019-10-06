da96f58236be2d7e70d507239ddd5b858d3701a6
Mikhail Delport <mikhail@delport.ca>
2019-10-06 18:48:36 -0400

    [WIP] Revert Phxauth

--- lib/budget_simple_web/router.ex
+++ lib/budget_simple_web/router.ex
@@ -6,8 +6,19 @@ defmodule BudgetSimpleWeb.Router do
     plug Phauxth.Authenticate, method: :token
   end
 
+  pipeline :auth do
+    plug :fetch_token_session
+  end
+
   scope "/api", BudgetSimpleWeb do
     pipe_through :api
+
+    resources "/users", UserController, only: [:create]
+  end
+
+  scope "/api", BudgetSimpleWeb do
+    pipe_through :api
+    pipe_through :auth
 
     post "/share", ShareController, :create
     post "/sessions", SessionController, :create
@@ -16,6 +27,19 @@ defmodule BudgetSimpleWeb.Router do
       resources "/categories", CategoryController, only: [:index, :create]
       resources "/accounts", AccountController, only: [:create]
       resources "/transactions", TransactionController, only: [:create]
+    end
+  end
+
+  defp fetch_token_session(conn, _) do
+    with true <- Kernel.is_bitstring(conn.params["token"]), {:ok, user} <- BudgetSimple.Accounts.get_session_user(conn.params["token"]) do
+      conn
+      |> assign(:current_user, user)
+    else
+      _ ->
+        conn
+        |> put_status(:forbidden)
+        |> render(BudgetSimpleWeb.ErrorView, "403.json", %{})
+        |> halt
     end
   end
 
